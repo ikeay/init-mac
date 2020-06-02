1cf721c8bef1a6a67a9e4ff2542de42b5e33ce4a
Ayaka Ikezawa <hello@ikeay.net>
2020-06-02 16:03:08 +0900

    replace tabs to spaces

--- link.sh
+++ link.sh
@@ -8,15 +8,15 @@ find ${backup_file_path}.ssh -type d -exec chmod 755 {} +
 find ${backup_file_path}.ssh -type f -exec chmod 600 {} +
 for file_path in $backup_file_path.*
 do
-	if [[ $file_path =~ ^$backup_file_path(.*)$ ]]; then
-		file=${BASH_REMATCH[1]}
-		case $file in
-	    	".") ;;
-			"..") ;;
-	        ".git") ;;
-	        *)
-				rm -rf $user_path$file
-				ln -s $backup_file_path$file $user_path$file;;
-    	esac
-	fi
+    if [[ $file_path =~ ^$backup_file_path(.*)$ ]]; then
+        file=${BASH_REMATCH[1]}
+        case $file in
+        ".") ;;
+        "..") ;;
+        ".git") ;;
+        *)
+            rm -rf $user_path$file
+            ln -s $backup_file_path$file $user_path$file;;
+        esac
+    fi
 done
